beca05d47ff888d35fd4d29a97976a89e6b7bfe1
Adam Jelinek <ajelinek@gmail.com>
2016-02-07 21:41:32 -0600

    Made changes to genereate the build folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 const TARGET = process.env.npm_lifecycle_event;
@@ -16,27 +17,30 @@ const common = {
     entry: {
         app: PATHS.app
     },
-    // Add resolve.extensions.
-    // '' is needed to allow imports without an extension.
-    // Note the .'s before extensions as it will fail to match without!!!
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ['', '.js', '.jsx'] // '' is needed to allow imports without an extension.
     },
     output: {
         path: PATHS.build,
         filename: 'bundle.js'
     },
-
     module: {
         loaders: [
             {test: /\.scss$/, loaders: ["style", "css", "sass"], include: PATHS.app},
             {test: /\.jsx?$/, loaders: ['babel?cacheDirectory'], include: PATHS.app}  //jsx and js
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'ReactJs-QuickStart',
+            template: 'index-template.ejs',
+            inject: 'body'
+        })
+    ]
 };
 
 //Target Represents the same name in
-if (TARGET === 'web-dev' || !TARGET) {
+if (TARGET === 'dev-web' || !TARGET) {
     module.exports = merge(common, {
         devtool: 'eval-source-map',
         devServer: {
